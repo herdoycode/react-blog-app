2de5b85bc042946be22412cae882fee885bdb174
Herdoy Almamun <herdoy1@gmail.com>
2023-01-27 14:29:41 +0600

    Completing login and signup page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="login">
+      <div className="login__box">
+        <h2>Login</h2>
+        <input type="text" placeholder="Enter your email..." />
+        <input type="text" placeholder="Enter your password..." />
+        <button> Login </button>
+        <button onClick={() => navigate("/signup")} className="btn__login">
+          Create Account
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Signup.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup">
+      <div className="signup__box">
+        <h2>Signup</h2>
+        <input type="text" placeholder="Full Name.." />
+        <input type="text" placeholder="Email Address..." />
+        <input type="text" placeholder="Password..." />
+        <button> Signup </button>
+        <Link to="/login">Already have Account? Login</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
